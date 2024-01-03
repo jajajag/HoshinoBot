04a02b596bf745df79fbf29597db51dea370ad39
Jag Zhang <jajajag@users.noreply.github.com>
2024-01-03 01:07:59 -0500

    Update comic.py

--- hoshino/modules/priconne/comic.py
+++ hoshino/modules/priconne/comic.py
@@ -2,6 +2,7 @@ import os
 import re
 import random
 import asyncio
+from bilibili_api import user
 from urllib.parse import urljoin, urlparse, parse_qs
 try:
     import ujson as json
@@ -12,18 +13,20 @@ from hoshino import aiorequests, R, Service
 from hoshino.typing import *
 
 sv_help = '''
-官方四格漫画更新(日文)
+官方四格漫画更新
 [官漫132] 阅览指定话
 '''.strip()
-sv = Service('pcr-comic', help_=sv_help, bundle='pcr订阅', enable_on_default=False)
+sv = Service('pcr-comic', help_=sv_help, bundle='pcr订阅', 
+             enable_on_default=False)
 
 def load_index():
-    with open(R.get('img/priconne/comic/index.json').path, encoding='utf8') as f:
+    with open(R.get('img/priconne/comic/index.json').path, 
+              encoding='utf8') as f:
         return json.load(f)
 
 def get_pic_name(id_):
     pre = 'episode_'
-    end = '.png'
+    end = '.jpg'
     return f'{pre}{id_}{end}'
 
 
@@ -38,11 +41,11 @@ async def comic(bot, ev: CQEvent):
         return
     index = load_index()
     if episode not in index:
-        await bot.send(ev, f'未查找到第{episode}话，敬请期待官方更新', at_sender=True)
+        await bot.send(ev, f'未查找到第{episode}话，敬请期待官方更新', 
+                       at_sender=True)
         return
-    title = index[episode]['title']
     pic = R.img('priconne/comic/', get_pic_name(episode)).cqcode
-    msg = f'プリンセスコネクト！Re:Dive公式4コマ\n第{episode}話 {title}\n{pic}'
+    msg = f'\n第{episode}话\n{pic}'
     await bot.send(ev, msg, at_sender=True)
 
 
@@ -102,31 +105,53 @@ async def update_seeker():
     轮询官方四格漫画更新
     若有更新则推送至订阅群
     '''
-    index_api = 'https://comic.priconne-redive.jp/api/index'
     index = load_index()
 
     # 获取最新漫画信息
-    resp = await aiorequests.get(index_api, timeout=10)
-    data = await resp.json()
-    id_ = data['latest_cartoon']['id']
-    episode = data['latest_cartoon']['episode_num']
-    title = data['latest_cartoon']['title']
+    # From 是年年嗷嗷嗷(https://space.bilibili.com/3260075/dynamic)
+    u = user.User(3260075)
+    res = sync(u.get_dynamics())
+    link = None
+    # Check the newest few dynamics
+    for card in res['cards']:
+        # Continue if no item/description/pictures in the dynamic
+        if 'item' not in card['card']: continue
+        item = card['card']['item']
+        if 'description' not in item or 'pictures' not in item: continue
+        if '四格漫画更新' not in item['description']: continue
+        rex = re.search(r'第\d+话', item['description'])
+        if rex is None: continue
+        # The newest comic dynamic found
+        episode = rex.group(0)[1:-1]
+        for picture in item['pictures']:
+            if picture['img_height'] > 512 and picture['img_width'] > 512:
+                link = picture['img_src']
+                break
+        if link is not None: break
+    # Return if no comic found in the dynamics
+    else: return
 
     # 检查是否已在目录中
     # 同一episode可能会被更新为另一张图片（官方修正），此时episode不变而id改变
     # 所以需要两步判断
     if episode in index:
-        qs = urlparse(index[episode]['link']).query
-        old_id = parse_qs(qs)['id'][0]
-        if id_ == old_id:
+        old_link = index[episode]
+        # Check if link has changed
+        if link == old_link:
             sv.logger.info('未检测到官漫更新')
             return
 
     # 确定已有更新，下载图片
     sv.logger.info(f'发现更新 id={id_}')
-    await download_comic(id_)
+    save_dir = R.img('priconne/comic/').path
+    await download_img(os.path.join(save_dir, get_pic_name(episode)), link)
+
+    # 保存官漫目录信息
+    index[episode] = link
+    with open(os.path.join(save_dir, 'index.json'), 'w', encoding='utf8') as f:
+        json.dump(index, f, ensure_ascii=False)
 
     # 推送至各个订阅群
     pic = R.img('priconne/comic', get_pic_name(episode)).cqcode
-    msg = f'プリンセスコネクト！Re:Dive公式4コマ更新！\n第{episode}話 {title}\n{pic}'
+    msg = f'\n第{episode}话\n{pic}'
     await sv.broadcast(msg, 'PCR官方四格', 0.5)

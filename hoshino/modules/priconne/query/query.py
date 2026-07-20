69645fcde1c41617a77365bdb291e4512767fdb4
Jag Zhang <jajajag@users.noreply.github.com>
2026-07-20 21:07:21 +0800

    Update query.py

--- hoshino/modules/priconne/query/query.py
+++ hoshino/modules/priconne/query/query.py
@@ -50,13 +50,6 @@ async def rank_sheet(bot, ev):
         await bot.send(ev, f"\nhttps://www.caimogu.cc/gzlj", at_sender=True)
 
 
-@sv.on_fullmatch('刷图')
-async def farm_sheet(bot, ev):
-    await bot.send(
-            ev, f"\nhttps://docs.qq.com/sheet/DYWxDbGdRYWV1bHFv?tab=r0h0ei",
-            at_sender=True)
-
-
 @sv.on_fullmatch('jjc', 'JJC', 'JJC作业', 'JJC作业网', 'JJC数据库', 'jjc作业', 'jjc作业网', 'jjc数据库')
 async def say_arina_database(bot, ev):
     await bot.send(ev, '公主连接Re:Dive 竞技场编成数据库\n日文：https://nomae.net/arenadb \n中文：https://pcrdfans.com/battle')
